69aa352b7b483adfe7409480845c7556ff79a5d9
JulioMartin12 <rl_yoo@yahoo.com.ar>
2026-09-07 20:10:48 -0300

    feat: Creando la funcionalidad  para asignar materias

--- frontend/src/app/views/admin/profesor/profesor.ts
+++ frontend/src/app/views/admin/profesor/profesor.ts
@@ -4,6 +4,8 @@ import { RouterModule} from '@angular/router';
 import { IPersona, Persona, RolId } from '../../../model/Persona.model';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProfesorService } from '../../../services/profesor.service';
+import { IMateria, IMateriaAsignacion } from '../../../model/materia.model';
+import { MateriaService } from '../../../services/materia.service';
 
 
 @Component({
@@ -14,12 +16,18 @@ import { ProfesorService } from '../../../services/profesor.service';
 })
 export class Profesor implements OnInit {
 private fb = inject(FormBuilder);
+private materiaService = inject(MateriaService);
 private profesorService = inject(ProfesorService)
 profesores = signal<Persona[]>([])
   isModalOpen = signal<boolean>(false);
   isEditing = signal<boolean>(false);
   selectedId = signal<number | null>(null);
   isLoading = signal<boolean>(false);
+  docenteParaAsignar = signal<Persona | null>(null);
+materiasDisponibles = signal<IMateria[]>([]);
+selectedMateriaIds = signal<number[]>([]);
+isLoadingMaterias = signal<boolean>(false);
+
 
   form = this.fb.nonNullable.group({
     nombre: ['', [Validators.required]],
@@ -104,12 +112,74 @@ profesores = signal<Persona[]>([])
   //   }
   }
 
-  asignar(profesor: Persona): void {
-   
+  asignar(docente: Persona): void {
+    console.log('Asignando materias al docente:', docente);
+    this.docenteParaAsignar.set(docente);
+    this.selectedMateriaIds.set([]);
+    this.isLoadingMaterias.set(true);
+
+    this.materiaService.cargarMateriasQueNoTengaElProfesor(this.docenteParaAsignar()!.id).subscribe({
+      next: (materias) => {
+        this.materiasDisponibles.set(materias);
+        this.isLoadingMaterias.set(false);
+      },
+      error: (err) => {
+        console.error('Error al cargar materias:', err);
+        this.isLoadingMaterias.set(false);
+      }
+    });
   }
+
    consultar(profesor: Persona): void {
    
   }
+
+  guardarAsignacion(): void {
+  const docente = this.docenteParaAsignar();
+  const ids = this.selectedMateriaIds();
+
+  if (!docente || ids.length === 0) return;
+this.materiaService.asignarProfesorAMaterias(docente.id, ids).subscribe({
+    next: (res) => {
+      console.log('Asignación completada:', res);
+      this.cerrarModalAsignar();
+  
+    },
+    error: (err) => {
+      console.error('Error al asignar materias al profesor:', err);
+    }
+  });
+}
+
+cerrarModalAsignar(): void {
+  this.docenteParaAsignar.set(null);
+  this.selectedMateriaIds.set([]);
+}
+
+
+toggleMateria(id: number | undefined): void {
+  if (!id) return;
+  this.selectedMateriaIds.update(current =>
+    current.includes(id) ? current.filter(item => item !== id) : [...current, id]
+  );
+}
+
+isMateriaSelected(id: number | undefined): boolean {
+  return id ? this.selectedMateriaIds().includes(id) : false;
+}
+
+seleccionarTodasMaterias(): void {
+  const todas = this.materiasDisponibles();
+  const idsValidos = todas
+    .map(m => m.id)
+    .filter((id): id is number => id !== undefined);
+
+  if (this.selectedMateriaIds().length === idsValidos.length) {
+    this.selectedMateriaIds.set([]);
+  } else {
+    this.selectedMateriaIds.set(idsValidos);
+  }
+}
 
 }
 
