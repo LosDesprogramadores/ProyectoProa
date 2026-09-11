4415df304f14449cff06b236813eed3d34f20360
AlandMarini <aland.marini@gmail.com>
2026-09-10 23:56:49 -0300

    feat(aula_virtual): Implementar models de Material, Actividad, Entrega y Nota. Fix(settings): .env para key

--- backend/proa/aula_virtual/models.py
+++ backend/proa/aula_virtual/models.py
@@ -1,3 +1,105 @@
 from django.db import models
+from usuario.models import Persona
+from academico.models import Materia
 
-# Create your models here.
+
+class Material(models.Model):
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='materiales')
+    titulo = models.CharField(max_length=150) 
+    descripcion = models.TextField(null=True, blank=True)
+    archivo = models.FileField(upload_to='materiales/%Y/%m/', null=True, blank=True)
+    enlace = models.URLField(max_length=500, blank=True, null=True)
+    formato = models.CharField(max_length=50, null=True,blank=True, help_text='Ej: PDF, Link o Zip')
+    visible = models.BooleanField(default=True)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'material'
+        verbose_name = 'Material'
+        verbose_name_plural = 'Materiales'
+        ordering = ['-fecha_publicacion']
+    
+    def __str__(self):
+        return f'{self.titulo} - {self.materia.titulo}'
+  
+
+class Actividad(models.Model):
+    class EstadoActividad(models.TextChoices):
+        BORRADOR = 'BORRADOR', "Borrador"
+        PUBLICADA = 'PUBLICADA', "Publicada"
+
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='actividades')
+    titulo = models.CharField(max_length=150)
+    descripcion = models.TextField(null=True, blank=True)
+    archivo_adjunto = models.FileField(upload_to='actividades/%Y/%m/', null=True,blank=True)
+    fecha_limite = models.DateTimeField()
+    permitir_entrega_tardia = models.BooleanField(default=True)
+    estado = models.CharField(max_length=50, choices=EstadoActividad.choices, default= EstadoActividad.PUBLICADA)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'actividad'
+        verbose_name = 'Actividad'
+        verbose_name_plural = 'Actividades'
+        ordering = ['-fecha_creacion']
+    
+    def __str__(self):
+        return f'{self.titulo} ({self.materia.titulo})'
+
+
+class Entrega(models.Model):
+    class EstadoEntrega(models.TextChoices):
+        BORRADOR = 'BORRADOR', 'Borrador'
+        ENTREGADO = 'ENTREGADO', 'Entregado'
+        CORREGIDO = 'CORREGIDO', 'Corregido'
+        REENTREGA = 'REENTREGA', 'Reentrega'
+
+    actividad = models.ForeignKey(Actividad, on_delete=models.CASCADE, related_name='entregas')
+    estudiante = models.ForeignKey('usuario.Persona', on_delete=models.CASCADE, related_name='entregas')
+    archivo = models.FileField(upload_to='entregas/%Y/%m/', null=True, blank=True)
+    enlace = models.URLField(max_length=500, null=True, blank=True)
+    contenido_texto = models.TextField(null=True, blank=True)
+    fuera_de_termino = models.BooleanField(default=False)
+    estado = models.CharField(
+        max_length=20,
+        choices=EstadoEntrega.choices,
+        default=EstadoEntrega.ENTREGADO
+    )
+    fecha_entrega = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'entrega'
+        verbose_name = 'Entrega'
+        verbose_name_plural = 'Entregas'
+        ordering = ['-fecha_entrega']
+        constraints = [models.UniqueConstraint(
+            fields=['actividad', 'estudiante'],
+            name='unique_entrega_actividad_estudiante'
+        )]
+
+    def __str__(self):
+        return f'Entrega: {self.estudiante.id} - {self.actividad.titulo}'
+
+
+
+class Nota(models.Model):
+    actividad = models.ForeignKey(Actividad, on_delete=models.CASCADE, related_name='notas')
+    estudiante = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='notas')
+    entrega = models.OneToOneField(Entrega, on_delete=models.SET_NULL, null=True,blank=True, related_name='nota')
+    profesor = models.ForeignKey(Persona, on_delete=models.SET_NULL, null=True, blank=True, related_name='notas_asentadas')
+    calificacion = models.DecimalField(max_digits=4, decimal_places=2, help_text='1 a 10')
+    descripcion = models.TextField(null=True,blank=True, help_text='Devolución o feedback del docente')
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        db_table = 'nota'
+        verbose_name = 'Nota'
+        verbose_name_plural = 'Notas'
+        constraints = [models.UniqueConstraint(
+            fields=['actividad', 'estudiante'],
+            name='unique_nota_actividad_estudiante'
+        )]
+
+    def __str__(self):
+        return f'{self.calificacion} | {self.estudiante.id} - {self.actividad.titulo}'
--- backend/proa/proa/settings.py
+++ backend/proa/proa/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +28,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i334sgi%7df-ij^xk%2!(wyaiv(h)$s_fa@p(5wm8gfbgfn2ji'
+SECRET_KEY = os.getenv("KEY_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
