7ab70ffe24f18ac0d6f2aebe7f11a327ef4f851a
JulioMartin12 <rl_yoo@yahoo.com.ar>
2026-09-11 23:41:19 -0300

    add: agregando los mensajes para para el usuario

--- frontend/src/app/views/admin/profesor/profesor.ts
+++ frontend/src/app/views/admin/profesor/profesor.ts
@@ -7,6 +7,7 @@ import { ProfesorService } from '../../../services/profesor.service';
 import { IMateria, IMateriaAsignacion } from '../../../model/materia.model';
 import { MateriaService } from '../../../services/materia.service';
 import { TablaGenerica } from '../tabla-generica/tabla-generica';
+import { ToastService } from '../../../services/toast.service';
 
 
 @Component({
@@ -19,6 +20,7 @@ export class Profesor implements OnInit {
 private fb = inject(FormBuilder);
 private materiaService = inject(MateriaService);
 private profesorService = inject(ProfesorService)
+private toastService = inject(ToastService)
 
 profesores = signal<Persona[]>([])
 isModalOpen = signal<boolean>(false);
@@ -65,6 +67,7 @@ columnasMaterias = [
           this.isLoading.set(false);
             console.log(data)},
       error: (err) => {console.error('Error al cargar profesores:', err )
+        this.toastService.error('Error al cargar profesores.');
         this.isLoading.set(false);
       }
       })
